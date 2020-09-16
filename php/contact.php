b1d4dddd749f586522dd9ef353364bb7b81e0c78
Pranjal <pranjalgupta362@gmail.com>
2020-09-16 15:07:51 +0530

    Updated Experience and modified contact form

---
+++ php/contact.php
@@ -0,0 +1,23 @@
+<?php
+
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $email_from = 'pranjalgupta363@gmail.com';
+
+    $email_body = "User Name: $name.\n". 
+                    "User Email: $visitor_email.\n". 
+                       "User Message: $message.\n";
+
+    $to = "pranjalgupta362@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply to: $visitor_email \r\n";
+
+    mail($to,$subject,$email_body,$headers);
+
+    header("Location: index.html");
+
+?>
